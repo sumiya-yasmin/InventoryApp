27dec48f73bf85db13862e3d1ed3a27885156cc5
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-04 16:08:55 +0600

    Add  basic static login function in controller

--- InventoryApp/Controllers/AuthController.cs
+++ InventoryApp/Controllers/AuthController.cs
@@ -7,6 +7,25 @@ namespace MyApp.Namespace
         // GET: AuthController
         public ActionResult Login()
         {
+             ViewBag.Message = "";
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Login(string emailtxt, string passwordtxt)
+        {
+            string Message = "Unauthorized";
+            if (emailtxt == "simi@nasif" && passwordtxt == "nasif")
+            {
+                Message = "Authorized";
+            }
+            ViewBag.Message = Message;
+            return View();
+
+        }
+
+        public ActionResult Signup()
+        {
             return View();
         }
 
