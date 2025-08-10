a29c7b23c985e1f758e9727195994036239e4f56
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-10 23:51:20 +0600

    Register DbContext in Program.cs

--- InventoryApp/Program.cs
+++ InventoryApp/Program.cs
@@ -1,9 +1,15 @@
+using InventoryApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
