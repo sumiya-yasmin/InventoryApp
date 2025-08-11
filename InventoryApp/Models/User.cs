8413a35a91ec6c7a6250f1905eece3d2d37f20ca
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-11 11:07:32 +0600

    Update User entity model

--- InventoryApp/Models/User.cs
+++ InventoryApp/Models/User.cs
@@ -6,5 +6,8 @@ public class User
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
-    
+    public string Role { get; set; } = "User";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? LastLogIn { get; set; }
+    public bool IsActive { get; set; } = true;
 }
