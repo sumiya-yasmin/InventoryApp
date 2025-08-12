ccd8eae1b6e1731158f58019866c26b838284390
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-12 22:21:52 +0600

    Add db context of added entity models

--- InventoryApp/Data/ApplicationDbContext.cs
+++ InventoryApp/Data/ApplicationDbContext.cs
@@ -5,8 +5,15 @@ namespace InventoryApp.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<Sale> Sales { get; set; }
     }
 }
