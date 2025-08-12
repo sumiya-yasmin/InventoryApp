13379ccb70705e61e398ce7dd10655109f9144ff
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-12 17:53:13 +0600

    Add Category entity model

---
+++ InventoryApp/Models/Category.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryApp.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+
+    [Required(ErrorMessage = "Category name is required")]
+    [MaxLength(100, ErrorMessage = "Category name cannot exceed 100 characters")]
+    public string Name { get; set; } = string.Empty;
+
+
+    [MaxLength(250)]
+    public string? Description { get; set; }
+
+    public ICollection<Product>? Products { get; set; }
+}
