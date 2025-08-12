d7d076443fe515abc01f3b1afc342b3e2459a86d
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-12 22:19:27 +0600

    Add Purchase entity model

---
+++ InventoryApp/Models/Purchase.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace InventoryApp.Models;
+
+public class Purchase
+{
+    public int PurchaseId { get; set; }
+    public DateTime PurchaseDate { get; set; } = DateTime.Now;
+
+    [Required]
+    public int Quantity { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal PricePerUnit { get; set; }
+
+    [Required]
+    public int ProductId { get; set; }
+    public required Product Product { get; set; }
+
+    [Required]
+    public int SupplierId { get; set; }
+    public Supplier Supplier { get; set; }
+
+}
