6ca15f135fd3e4cfa6a29c7b7e5a4b17f8d70c4d
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-13 23:59:22 +0600

    Add Purchase Services

---
+++ InventoryApp/Services/Implementations/PurchaseService.cs
@@ -0,0 +1,58 @@
+using InventoryApp.Data;
+using InventoryApp.Models;
+using InventoryApp.Services.Interfaces;
+using Microsoft.EntityFrameworkCore; 
+
+namespace InventoryApp.Services
+{
+    public class PurchaseService : IPurchaseService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PurchaseService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Purchase>> GetAllPurchaseAsync()
+        {
+            return await _context.Purchases
+                .Include(p => p.Product) // ✅ Load related Product
+                .ToListAsync();
+        }
+
+        public async Task<Purchase> GetPurchaseByIdAsync(int id)
+        {
+            return await _context.Purchases
+                .Include(p => p.Product)
+                .FirstOrDefaultAsync(p => p.PurchaseId == id);
+        }
+
+        public async Task<Purchase> AddPurchaseAsync(Purchase purchase)
+        {
+            _context.Purchases.Add(purchase);
+            await _context.SaveChangesAsync();
+            return purchase;
+        }
+
+        public async Task<Purchase> UpdatePurchaseAsync(Purchase purchase)
+        {
+            _context.Purchases.Update(purchase);
+            await _context.SaveChangesAsync();
+            return purchase;
+        }
+
+        public async Task<Purchase> DeletePurchaseAsync(int id)
+        {
+            var purchase = await _context.Purchases.FindAsync(id);
+            if (purchase != null)
+            {
+                _context.Purchases.Remove(purchase);
+                await _context.SaveChangesAsync();
+            }
+            return purchase;
+        }
+    }
+}
+
+
