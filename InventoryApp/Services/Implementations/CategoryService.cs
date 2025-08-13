c3177642fde07afbf8ca97056943218d9abb2958
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-13 19:31:25 +0600

    Add Category Service

---
+++ InventoryApp/Services/Implementations/CategoryService.cs
@@ -0,0 +1,54 @@
+using InventoryApp.Data;
+using InventoryApp.Models;
+using InventoryApp.Services.interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApp.Services.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetCategoryByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task<Category> AddCategoryAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+            
+        }
+
+        public async Task<Category> UpdateCategoryAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category?>  DeleteCategoryAsync(int id) {
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();  
+            }
+                return category;
+
+        }
+
+    }
+}
