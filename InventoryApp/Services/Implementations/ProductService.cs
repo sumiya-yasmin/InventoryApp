507ce89899dfd0ea6395abfe0f1047857f1d894b
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-13 23:46:00 +0600

    Add Product Services

---
+++ InventoryApp/Services/Implementations/ProductService.cs
@@ -0,0 +1,42 @@
+using InventoryApp.Data;
+using InventoryApp.Models;
+using InventoryApp.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApp.Services.Implementations;
+
+public class ProductService : IProductService
+{
+    private readonly ApplicationDbContext _context;
+    public ProductService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<Product>> GetAllProductAsync() => await _context.Products.Include((p) => p.Category).ToListAsync();
+    public async Task<Product?> GetProductByIdAsync(int id) => await _context.Products.Include((p) => p.Category).FirstOrDefaultAsync((p) => p.ProductId == id);
+    public async Task<Product> AddProductAsync(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return product;
+
+    }
+
+    public async Task<Product> UpdateProductAsync(Product product)
+    {
+        _context.Products.Update(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product?> DeleteProductAsync(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product != null)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+        return product;
+    }
+}
