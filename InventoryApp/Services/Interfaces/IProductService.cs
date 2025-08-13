ed02cc0ed622e7cdfdff41a9a16b48150ad8a74f
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-13 19:31:56 +0600

    Add Product Service interface

---
+++ InventoryApp/Services/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+using InventoryApp.Models;
+namespace InventoryApp.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProductAsync();
+        Task<Product?> GetProductByIdAsync(int id);
+        Task<Product> AddProductAsync(Product product);
+        Task<Product> UpdateProductAsync(Product product);
+        Task<Product?> DeleteProductAsync(int id);
+
+    }
+}
