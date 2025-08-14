d1d94f1a9dcad2cc1b1545dae5aad09e8f8c587e
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-14 18:26:33 +0600

    Register the services in Program.cs

--- InventoryApp/Program.cs
+++ InventoryApp/Program.cs
@@ -1,10 +1,24 @@
 using InventoryApp.Data;
+using InventoryApp.Services.Implementations;
+using InventoryApp.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<ISupplierService, SupplierService>();
+builder.Services.AddScoped<ISaleService, SaleService>();
+builder.Services.AddScoped<IPurchaseService, PurchaseService>();
+
+
+
+
+
+
 builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
