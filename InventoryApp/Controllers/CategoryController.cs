140733e54740e489a01b8eda35a5f83dfd883ca7
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-14 20:51:44 +0600

    Add complete Category Controller

---
+++ InventoryApp/Controllers/CategoryController.cs
@@ -0,0 +1,125 @@
+using InventoryApp.Models;
+using InventoryApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApp.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ICategoryService _categoryService;
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var categories = await _categoryService.GetAllCategoriesAsync();
+        return View(categories);
+    }
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var category = await _categoryService.GetCategoryByIdAsync(id.Value);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("name, Description")] Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            await _categoryService.AddCategoryAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+
+
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+
+        }
+        var category = await _categoryService.GetCategoryByIdAsync(id.Value);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("CategoryId,Name,Description")] Category category)
+    {
+        if (id != category.CategoryId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await _categoryService.UpdateCategoryAsync(category);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (await _categoryService.GetCategoryByIdAsync(category.CategoryId) == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var category = await _categoryService.GetCategoryByIdAsync(id.Value);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _categoryService.DeleteCategoryAsync(id);
+        return RedirectToAction(nameof(Index));
+    }
+
+}
