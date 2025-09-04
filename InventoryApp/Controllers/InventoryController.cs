358c6521701e5e4405b7dea94722bf5659f77ffd
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-04 23:36:23 +0600

    Add inventory controller for dashboard view page

--- InventoryApp/Controllers/InventoryController.cs
+++ InventoryApp/Controllers/InventoryController.cs
@@ -1,14 +1,62 @@
+using InventoryApp.Services.Interfaces;
+using InventoryApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyApp.Namespace
 {
     public class InventoryController : Controller
     {
-        // GET: InventoryController
-        public ActionResult Dashboard()
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+        private readonly ICustomerService _customerService;
+        private readonly ISupplierService _supplierService;
+        private readonly ISaleService _saleService;
+        private readonly IPurchaseService _purchaseService;
+
+        public InventoryController(
+            ICategoryService categoryService,
+            IProductService productService,
+           ICustomerService customerService,
+            ISupplierService supplierService,
+            ISaleService saleService,
+            IPurchaseService purchaseService
+        )
         {
-            return View();
+            _categoryService = categoryService;
+            _productService = productService;
+            _customerService = customerService;
+            _supplierService = supplierService;
+            _saleService = saleService;
+            _purchaseService = purchaseService;
         }
+
+
+        public async Task<IActionResult> Dashboard()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+            var products = await _productService.GetAllProductAsync();
+            var customers = await _customerService.GetAllCustomerAsync();
+            var suppliers = await _supplierService.GetAllSupplierAsync();
+            var sales = await _saleService.GetAllSaleAsync();
+            var purchases = await _purchaseService.GetAllPurchaseAsync();
+            var model = new DashboardViewModel
+            {
+                Categories = categories,
+                Products = products,
+                Customers = customers,
+                Suppliers = suppliers,
+                Sales = sales,
+                Purchases = purchases
+
+            };
+            return View(model);
+        }
+        
+
+        // public ActionResult Dashboard()
+        // {
+        //     return View();
+        // }
 
     }
 }
